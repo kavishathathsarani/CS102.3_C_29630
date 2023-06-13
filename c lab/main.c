2a22af0ee8193ebd5fc778e20ae6d940dee954f4
kavishathathsarani <135618896+kavishathathsarani@users.noreply.github.com>
2023-06-13 16:25:45 +0530

    Add files via upload

---
+++ c lab/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int no1,no2,total;
+    printf("Enter first number ");
+    scanf("%d",&no1);
+    printf("Enter second number ");
+    scanf("%d",&no2);
+    total=no1+no2;
+    printf("The total is %d \n",total);
+    return 0;
+}
